0984aa7fa3e9e1c0fb438fcb5f50c39214ce7f92
alunogtisenac <57047762+alunogtisenac@users.noreply.github.com>
2021-05-30 10:23:21 -0300

    Delete Serviços em Nuvem directory

--- Serviços em Nuvem/src/App.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import Cadastro from "./components/Cliente/Cadastro";
-
-export default class App extends Component {
-  render() {
-    return <Cadastro />;
-  }
-}
